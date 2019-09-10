c723c86bb25bab9d85445d4adcd1d47df2246557
Ravi Rathore <ravirathore15@icloud.com>
2019-09-10 22:07:22 +1000

    completed product list

---
+++ src/components/Product.js
@@ -0,0 +1,42 @@
+import React, { Component } from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+import { ProductConsumer } from "../context";
+
+export default class Product extends Component {
+  render() {
+    const { id, title, img, price, inCart } = this.props.product;
+    return (
+      <ProductWrapper className="col-9 mx-auto col-md-6 cold-lg-3 my-3">
+        <div className="card">
+          <div
+            className="img-container p-5"
+            onClick={console.log("you clicked ")}
+          >
+            <Link to="/details">
+              <img src={img} alt="product" className="card-img-top"></img>
+            </Link>
+            <button
+              className="cart-btn"
+              disabled={inCart ? true : false}
+              onClick={() => {
+                console.log("added to the cart");
+              }}
+            >
+              {inCart ? (
+                <p className="text-capitalize mb-0" disabled>
+                  {" "}
+                  In Cart
+                </p>
+              ) : (
+                <i className="fas fa-cart-plus"></i>
+              )}
+            </button>
+          </div>
+        </div>
+      </ProductWrapper>
+    );
+  }
+}
+
+const ProductWrapper = styled.div``;
